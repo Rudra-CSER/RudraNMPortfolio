2e4046737071e9a5364508964a527a5c82674cb6
Rudra-CSER <djshina1900@gmail.com>
2026-01-21 18:49:45 +0530

    file structure and mac-book style dock uploded

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './app.scss'
+import Doc from './components/doc'
+function App() {
+
+
+return (
+    <>
+      <main className="main">
+         <Doc/>
+      </main>
+    </>
+  )
+}
+
+export default App
